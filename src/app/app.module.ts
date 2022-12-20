eed59303a5996e944170cc172114f000061310a0
Onkar Patil <2020bcs504@sggs.ac.in>
2022-12-21 00:04:36 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Results } from './result.component';
+import { Student } from './student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Student,
+    Results
   ],
   imports: [
     BrowserModule
